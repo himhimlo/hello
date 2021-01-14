587da4af7c875927dc179b11104428cd160af53f
himhimlo <himhimlo126@gmail.com>
2021-01-14 10:11:09 +0800

    Simplify some functional code

--- src/main/java/com/siuhim/hello/security/MyUserDetailsService.java
+++ src/main/java/com/siuhim/hello/security/MyUserDetailsService.java
@@ -19,7 +19,8 @@ public class MyUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<User> user = userRepository.findByUsername(username);
-        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + username));
-        return user.map(MyUserDetails::new).get();
+        return user
+                .map(MyUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("Not found: " + username));
     }
 }
